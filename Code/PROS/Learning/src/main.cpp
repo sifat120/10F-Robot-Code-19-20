df7c8304b383fe45c33dbb3637bbd3d31cdbffb3
Sifat Hasan <sifat120@outlook.com>
2020-03-26 11:49:47 -0700

    Switched to using namespace pros which is more useful to code in

--- Code/PROS/Learning/src/main.cpp
+++ Code/PROS/Learning/src/main.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 
+using namespace pros;
+
 /**
  * A callback function for LLEMU's center button.
  *
@@ -10,9 +12,9 @@ void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
 	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
+		lcd::set_text(2, "I was pressed!");
 	} else {
-		pros::lcd::clear_line(2);
+		lcd::clear_line(2);
 	}
 }
 
@@ -23,10 +25,10 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+	lcd::initialize();
+	lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
+	lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -74,19 +76,19 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	Controller master(pros::E_CONTROLLER_MASTER);
+	Motor left_mtr(1);
+	Motor right_mtr(2);
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		lcd::print(0, "%d %d %d", (lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+		                 (lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+		                 (lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
 		left_mtr = left;
 		right_mtr = right;
-		pros::delay(20);
+		delay(20);
 	}
 }
